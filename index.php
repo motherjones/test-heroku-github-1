46d79e5099e1d8f1ab155890ba265b62847565c5
Young Kim <ykim@motherjones.com>
2022-03-24 16:59:57 -0700

    troubleshooting

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 header("Access-Control-Allow-Origin: http://util.motherjones.net");
 
+if($_SERVER['HTTP_REFERER'],"airtableblocks.com") === true) {
+    $traceback = $_SERVER['HTTP_REFERER'];
+}
+
+echo $traceback;
 
  ?>

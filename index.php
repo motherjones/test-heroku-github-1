c8e55673407de8cb2ea9805472ac6fe3faf1f951
Young Kim <ykim@motherjones.com>
2022-03-25 11:59:38 -0700

    more frying tests

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-if(stripos($_SERVER['HTTP_REFERER'], "airtable.com") === false)) {
-    echo "Unacceptable domain";
-    die();
-}
-else {
-    echo "I am here. I am born. I am crying. I am... free...";    
-}
+$traceback = $_SERVER['HTTP_REFERER'];
+
+echo "I am here. I am born. I am crying. I am... free...";
 ?>

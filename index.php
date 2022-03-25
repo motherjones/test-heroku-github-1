6cfd22b5f5e129d38b0b0bfc74e37accc031c047
Young Kim <ykim@motherjones.com>
2022-03-24 17:26:05 -0700

    neverending story

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 if(stripos($_SERVER['HTTP_REFERER'],"motherjones.net") !== false)) {
     $traceback = $_SERVER['HTTP_REFERER'];
-    header("Access-Control-Allow-Origin: $traceback");
 }
+
+header("Access-Control-Allow-Origin: $traceback");
 
 echo $traceback;
 

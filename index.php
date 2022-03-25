cd81edd1b2a741aca1f9e7482a8870103260e1c0
Young Kim <ykim@motherjones.com>
2022-03-25 10:19:49 -0700

    testing testing

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 if(stripos($_SERVER['HTTP_REFERER'],"motherjones.net")!==false)) {$traceback=$_SERVER['HTTP_REFERER'];}
-header("Access-Control-Allow-Origin: $traceback");
 
 echo $traceback;
 ?>

bf4db2b4db65fcf5eef14062032fcda92bbaac7e
Young Kim <ykim@motherjones.com>
2022-03-25 11:02:31 -0700

    this is another try

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 header("Access-Control-Allow-Origin: $_SERVER['HTTP_REFERER']");
 
-if(stripos($_SERVER['HTTP_REFERER'], "motherjones.net") === false)) {
+/*if(stripos($_SERVER['HTTP_REFERER'], "motherjones.net") === false)) {
     echo "Unacceptable domain";
     die();
-}
+}*/
 
 echo $traceback;
 ?>

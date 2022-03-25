c6b0ebe3623591f968b227a890ba7979b84657d3
Young Kim <ykim@motherjones.com>
2022-03-25 14:06:41 -0700

    testing on all testing platforms

--- index.php
+++ index.php
@@ -3,8 +3,10 @@ header("Access-Control-Allow-Origin: *");
 
 $traceback = $_SERVER['HTTP_REFERER'];
 
-if(stripos($traceback, "airtable.com") === false) {
-    echo "Not allowed";
+if(stripos($traceback, "airtableblocks.com") === false) {
+    console.log($traceback);
+    $err_msg["message"] = "Not allowed"; 
+    echo json_encode($err_msg);
     die();
 }
 

4fa769e4c376019771470da0e8e40612ab127dc6
Young Kim <ykim@motherjones.com>
2022-03-25 12:01:14 -0700

    more testing things

--- index.php
+++ index.php
@@ -3,5 +3,13 @@ header("Access-Control-Allow-Origin: *");
 
 $traceback = $_SERVER['HTTP_REFERER'];
 
-echo "I am here. I am born. I am crying. I am... free...";
+
+if(stripos($traceback, "motherjones.net")) {
+    echo "I am here. I am born. I am crying. I am... free...";
+}
+else {
+    echo "Not allowed";
+}
+
+
 ?>

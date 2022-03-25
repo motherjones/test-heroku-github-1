a801b57e06919bb80eaeeddf2f0fb1115f57214d
Young Kim <ykim@motherjones.com>
2022-03-24 17:03:51 -0700

    I hate syntax errors that give no errors

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: http://util.motherjones.net");
 
-if($_SERVER['HTTP_REFERER'],"airtableblocks.com") === true) {
+if(stripos($_SERVER['HTTP_REFERER'],"airtableblocks.com") === true)) {
     $traceback = $_SERVER['HTTP_REFERER'];
 }
 

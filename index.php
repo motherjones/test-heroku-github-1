44fcecb1de3fa71aa8ffae291188d1d1ad9fbc61
Young Kim <ykim@motherjones.com>
2022-03-25 09:42:46 -0700

    trying something else

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 if(stripos($_SERVER['HTTP_REFERER'],"motherjones.net") !== false)) {
     $traceback = $_SERVER['HTTP_REFERER'];
 }
-
 header("Access-Control-Allow-Origin: $traceback");
 
 echo $traceback;
 
- ?>
+?>
